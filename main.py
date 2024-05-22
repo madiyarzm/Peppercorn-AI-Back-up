455afb4628a25419ebf71e53efc37b8353a3db36
madiyarzm <148677506+madiyarzm@users.noreply.github.com>
2024-05-22 15:48:55 +0500

    main.py static files
    imported libraries to download static files

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from routers.login import login_router
 from routers.upload_csv_data import upload_data_router
 from routers.retrieve_csv_data import retrieve_data_router
 from routers.email_sending import sending_emails_router
+from fastapi.staticfiles import StaticFiles
+from fastapi import File, UploadFile
+from fastapi.responses import JSONResponse
+import shutil
+
 
 app = FastAPI()
 
@@ -11,8 +16,18 @@ app.include_router(upload_data_router)
 app.include_router(retrieve_data_router)
 app.include_router(sending_emails_router)
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+@app.post("/upload-csv-data")
+async def upload_csv_data(file: UploadFile = File(...)):
+    try:
+        with open(f"uploads/{file.filename}", "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        return JSONResponse(content={"message": "File uploaded successfully"})
+    except Exception as e:
+        return JSONResponse(content={"message": "File upload failed"}, status_code=500)
